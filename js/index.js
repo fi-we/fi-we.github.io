8aa92fe5a9a6eb837be170bd441b0619efe3f042
Денис <103744417+fi-we@users.noreply.github.com>
2022-05-12 17:24:39 +0300

    Ремейк сайта

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const phrases = ["Привет", "Я fiWe", "Добро пожаловать на мою страницу GitHub"], speed = 90;
+let phrase;
+
+function wordflick() {
+    
+}
+
+$(document).ready(() => {
+    wordflick();
+    console.log("Привет, как жизнь? :)");
+});
